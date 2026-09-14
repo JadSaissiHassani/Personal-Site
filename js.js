16a9fb957384604d3e695b902ec15cf57396f3e1
JadSaissiHassani <jadsaissihassani37@gmail.com>
2026-09-14 21:13:52 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+// Smooth scroll for nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+ 
+// Simple console greeting
+console.log("Welcome to DTFive's personal site!");
+ 
